912bb44007bc2318ebb3e1cc69a9d633848a67bf
stesh <ymsil719@gmail.com>
2016-12-08 16:59:46 +0200

    started contentProvider

---
+++ app/src/main/java/trabelstesh/javaproject/model/backend/MyContentProvider.java
@@ -0,0 +1,50 @@
+package trabelstesh.javaproject.model.backend;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ymsil on 12/8/2016.
+ */
+
+public class MyContentProvider extends ContentProvider
+{
+    DB_manager manager = DBManagerFactory.getManager();
+    final String TAG = "ProjectContemt";
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
