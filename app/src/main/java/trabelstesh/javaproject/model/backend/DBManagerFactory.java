10bdb313e93d5cf314c7d48acbff0188a403ba6a
stesh <ymsil719@gmail.com>
2016-12-08 16:42:35 +0200

    reorganized project

--- app/src/main/java/trabelstesh/javaproject/model/backend/DBManagerFactory.java
+++ app/src/main/java/trabelstesh/javaproject/model/backend/DBManagerFactory.java
@@ -1,17 +1,18 @@
 package trabelstesh.javaproject.model.backend;
 
+import trabelstesh.javaproject.model.datasource.List_DBManager;
+
 /**
  * Created by ymsil on 12/8/2016.
  */
 
 public class DBManagerFactory
 {
-    static DB_manager db = null;
-    public static DB_manager getDb()
-    {
-        ///DAL layer implentation using a singelton
-        if (db != null) return db;
-        db = new DB();
-        return db;
+    static DB_manager manager = null;
+
+    public static DB_manager getManager() {
+        if (manager == null)
+            manager = new List_DBManager();
+        return manager;
     }
 }
--- app/src/main/java/trabelstesh/javaproject/model/datasource/List_DBManager.java
+++ app/src/main/java/trabelstesh/javaproject/model/datasource/List_DBManager.java
@@ -1,10 +1,67 @@
 package trabelstesh.javaproject.model.datasource;
 
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import trabelstesh.javaproject.model.backend.DB_manager;
+import trabelstesh.javaproject.model.entities.Business;
+import trabelstesh.javaproject.model.entities.User;
+
 /**
  * Created by ymsil on 12/8/2016.
  */
 
-public class List_DBManager
+public class List_DBManager implements DB_manager
 {
+    static List<User> users;
+    static List<Business> businesses;
+    static List<Activity> activities;
 
+    static {
+        users = new ArrayList<>();
+        businesses = new ArrayList<>();
+        activities = new ArrayList<>();
+    }
+
+    @Override
+    public void AddUser() {
+
+    }
+
+    @Override
+    public void AddBuisness() {
+
+    }
+
+    @Override
+    public void AddActivity() {
+
+    }
+
+    @Override
+    public boolean IsNewActivityOrBuisness() {
+        return false;
+    }
+
+    @Override
+    public ArrayList<User> GetAllUsers() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Business> GetAllBusinesses() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Activity> GetAllActivities() {
+        return null;
+    }
+
+    @Override
+    public boolean isNewData() {
+        return false;
+    }
 }
